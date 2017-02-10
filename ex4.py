bf9ae34b2cf1779b7cc33c406b88794aac5fc99d
Ruben van Smirren <ruben.van.smirren@getbynder.com>
2017-02-10 11:21:23 +0100

    Adding excercises

---
+++ ex4.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+#Create a program that asks the user for a number and then prints out a list of all the divisors of that number. 
+#--------------------------------------------------------------------------------------------------------------
+
+import math
+
+number = int(raw_input("Please enter a number: "))
+if number 	
+
+
+
+
+
